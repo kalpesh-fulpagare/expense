3b6a6b85e3b03ded5c5c418d49fdafe79e1ab1f1
Kalpesh Fulpagare <kalpesh.fulpagare@bizcatalyst.co.in>
2014-03-03 18:57:50 +0530

    Updated host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ RailsApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'expense-kalpeshonrails.rhcloud.com' }
+  config.action_mailer.default_url_options = { :host => 'expense.kalpeshonrails.in' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mailgun.org',
